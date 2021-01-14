7fe26759399d98997d29a0fba26c9790528b8372
Bob Jordon <70407137+Two-Trees@users.noreply.github.com>
2021-01-14 14:49:27 -0500

    Add files via upload

---
+++ mario/mario.c
@@ -0,0 +1,47 @@
+// teachtolearn.life
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+   
+    do
+    {
+        n = get_int("how would you like to cut through th bullshit??\n");
+    } while (n < 1 || n > 8);
+    
+    
+    // declares variable i 
+    for(int i = 0;  i < n; i++)
+    {
+        // declares variable j 
+        for(int j = 0; j < n; j++)
+        {
+            //creates conditions for printing hashes and spaces
+            if(i + j < n - 1) 
+            {
+                printf(" ");
+            }
+            else 
+            {
+                printf("#");
+            
+                if(j == n - 1)
+                {
+                    printf("  ");
+                
+                    for(int k = 0; k < i + 1; k++)
+                    {
+                        printf("#");
+                    }
+                }
+            }
+        }
+        
+        printf("\n");
+    }
+}
+
+//check us out: www.teachtolearn.life
